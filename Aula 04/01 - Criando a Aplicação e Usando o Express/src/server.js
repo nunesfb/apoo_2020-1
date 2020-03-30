0e05dd0d73f4c858bab87f0c39bbb49a92cd00ef
Felipe Becker Nunes <35842441+nunesfb@users.noreply.github.com>
2020-03-30 17:35:35 -0300

    remove

--- Aula 04/01 - Criando a Aplicação e Usando o Express/src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app.js');
-
-// aqui eu inicio o servidor
-// com essa divisão não preciso ter a execução do servidor junto
-// isso ajuda bastante nos testes automatizados
-app.listen(3000);
